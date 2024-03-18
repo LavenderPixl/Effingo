b7b7f342dcdfaacee3ca0b3c9f7b0c3ccc882fc7
LavenderPixl <annajacobsen1999@hotmail.com>
2024-03-18 13:05:56 +0100

    Asset Testing

--- scenes/FirstLevel.gd
+++ scenes/FirstLevel.gd
@@ -25,21 +25,21 @@ func _on_pink_gem_collected():
 	$Player2.score += 1	
 	$HUD.setPink($Player2.score, pink_max)
 
-#func _on_area_2d_body_entered(body):
-	#if body == $Player2:
-		#pink_done = true
-	#if body == $Player:
-		#blue_done = true
-	#
-	#print(body)
-	#print(pink_done)
-#
-#func _on_area_2d_body_exited(body):
-	#if body == $Player2:
-		#pink_done = false
-	#if body == $Player:
-		#blue_done = false
-	#
-	#print(body)
-	#print(pink_done)
+func _on_area_2d_body_entered(body):
+	if body == $Player2:
+		pink_done = true
+	if body == $Player:
+		blue_done = true
+	
+	print(body)
+	print(pink_done)
+
+func _on_area_2d_body_exited(body):
+	if body == $Player2:
+		pink_done = false
+	if body == $Player:
+		blue_done = false
+	
+	print(body)
+	print(pink_done)
 
